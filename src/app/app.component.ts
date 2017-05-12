6ba510f62e4dd444a2e9a6f95e912d2a57d25cf9
Jacob Hixon <jacobhixon47@gmail.com>
2017-05-12 01:57:23 -0700

    add styles/nav fade/cool looks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { HostListener} from "@angular/core";
+import { DOCUMENT } from "@angular/platform-browser";
+import { Inject } from "@angular/core";
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
+
 })
 export class AppComponent {
+  public navIsShown: boolean = true;
+  public navIsSolid: boolean = false;
+  public scrollPosition = this.document.body.scrollTop;
+  constructor(@Inject(DOCUMENT) private document: Document) {}
   title = 'app works!';
+  @HostListener("window:scroll", [])
+    isNavShown() {
+      let number = this.document.body.scrollTop;
+      if (number > this.scrollPosition) {
+        this.navIsShown = false;
+      } else if (!this.navIsShown && number < this.scrollPosition) {
+        this.navIsShown = true;
+      }
+      this.scrollPosition = number;
+    }
+
 }
